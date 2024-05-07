810a338e0525e3c08b93f65067a88df01b4150d9
root <root@ARTHUR>
2024-05-07 10:00:49 +0700

    fresh from the oven

---
+++ pert4/while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+//maksudnya gimana?
+void dowhile(){
+    do(
+        cout << a << endl;
+        a = a+1;
+    )
+}
+
+int main()
+penggunaandowhile();
+penggunaan 
